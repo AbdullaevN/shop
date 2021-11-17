7480ab34efeb75d0f7b2fe2d57558592d1ec4723
nno <nurlanabdullaev9820@gmail.com>
2021-11-17 14:55:23 +0600

    addedCart

--- src/Pages/AdminPage.jsx
+++ src/Pages/AdminPage.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import BasicTable from "../components/BasicTable/BasicTable";
+import CatalogPage from "./CatalogPage";
 
 const AdminPage = () => {
-  return <div>ADMINPAGE</div>;
+  return (
+    <>
+      <div>ADMINPAGE</div>
+      <BasicTable />
+    </>
+  );
 };
 
 export default AdminPage;
